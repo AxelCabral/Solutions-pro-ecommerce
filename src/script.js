09dab118acb72487372d86a2bbf69c08d91e29bd
Axel Cabral <axel.cabral0102@gmail.com>
2025-03-02 22:08:09 -0300

    v0.4

--- src/script.js
+++ src/script.js
@@ -1,16 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const buyButton = document.getElementById("buyNow");
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grapCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
 
-  buyButton.addEventListener("mouseover", function() {
-      buyButton.style.transform = "scale(1.1)";
-  });
-
-  buyButton.addEventListener("mouseleave", function() {
-      buyButton.style.transform = "scale(1)";
-  });
-
-  buyButton.addEventListener("click", function() {
-      alert("Thank you for your interest! Redirecting to checkout...");
-      // Here you can add actual redirection logic
-  });
+    breakpoints:{
+        0: {
+            slidesPerView: 1,  
+        },
+        520: {
+            slidesPerView: 2,  
+        },
+        950: {
+            slidesPerView: 4,  
+        }
+    }
 });
